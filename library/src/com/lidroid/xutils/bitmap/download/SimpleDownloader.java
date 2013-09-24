631fdd013cbd14de7d9f227f0c46d6f59af028fc
Dong Weiqiang <weiqiang@teambition.com>
2013-09-24 20:44:35 +0800

    add https support

--- library/src/com/lidroid/xutils/bitmap/download/SimpleDownloader.java
+++ library/src/com/lidroid/xutils/bitmap/download/SimpleDownloader.java
@@ -18,11 +18,13 @@ package com.lidroid.xutils.bitmap.download;
 import com.lidroid.xutils.util.IOUtils;
 import com.lidroid.xutils.util.LogUtils;
 
+import javax.net.ssl.*;
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.OutputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.security.cert.X509Certificate;
 
 
 public class SimpleDownloader implements Downloader {
@@ -41,6 +43,33 @@ public class SimpleDownloader implements Downloader {
 
         long result = -1;
         try {
+
+            // Create a trust manager that does not validate certificate chains
+            TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+            } };
+            // Install the all-trusting trust manager
+            final SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            // Create all-trusting host name verifier
+            HostnameVerifier allHostsValid = new HostnameVerifier() {
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            };
+
+            // Install the all-trusting host verifier
+            HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+
+
+
             if (uri.startsWith("/")) {
                 FileInputStream fileInputStream = new FileInputStream(uri);
                 bis = new BufferedInputStream(fileInputStream);
